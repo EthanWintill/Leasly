27ddaa9d926f5b3cfc44167a24508f4b3894169e
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-03-15 00:37:14 -0500

    Add files

---
+++ routes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python -B
+import sys 
+sys.dont_write_bytecode = True
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html') 
+
+@app.route('/listing')
+def listing():
+  return render_template('listing.html')
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
