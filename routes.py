3d0d7d180e34f5a4367311ad88c63e1b5bc1185e
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-03-15 01:56:56 -0500

    Added form for new listing with basic functionality

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 import sys 
+import os
 sys.dont_write_bytecode = True
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -12,6 +13,23 @@ def home():
 def listing():
   return render_template('listing.html')
 
+@app.route('/form')
+def form():
+  return render_template('newListForm.html')
+
+@app.route('/create_listing', methods=['POST'])
+def create_listing():
+    beds = request.form['beds']
+    baths = request.form['baths']
+    sqft = request.form['sqft']
+    location = request.form['location']
+    rent = request.form['rent']
+    description = request.form['description']
+    image_file = request.files['image']
+    image_filename = image_file.filename
+    image_path =os.path.join('static/img', image_filename)
+    image_file.save(image_path)
+    return render_template('listing.html', beds=beds, baths=baths, sqft=sqft, location=location, rent=rent, description=description, filename=image_filename)
 
 
 
