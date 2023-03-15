6c6e747da866f5e1c1feb92a2ab4facca27e03ea
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-03-15 02:46:46 -0500

    Added more bare bones functionality for adding and viewing listings
    Right now I have them stored in a .py file, will eventually need to use a database. Once we have a home button, should finish absolute bare bones

---
+++ data/storedListings.py
@@ -0,0 +1,22 @@
+
+listings = [
+    {
+        'description': 'This studio apartment is perfect for a single person or couple. It features a comfortable bed, a fully equipped kitchen, and a modern bathroom. Located in a quiet neighborhood with easy access to public transportation.',
+        'bedrooms': 1,
+        'bathrooms': 1,
+        'rent':1000,
+        'location':"san marcos",
+        'sqft': 500,
+        'image': 'AIroom.png'
+    },
+    {
+        'description': 'This beautiful house is perfect for families or groups. It features three bedrooms, two bathrooms, a large living room, and a fully equipped kitchen. The house is located in a quiet, tree-lined neighborhood with easy access to shopping and dining.',
+        'bedrooms': 3,
+        'bathrooms': 2,
+        'rent':5000,
+        'location':'houston',
+        'sqft': 2000,
+        'image': 'AIroom.png'
+    },
+    # More listings go here
+]
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 import sys 
 import os
+from data import storedListings
 sys.dont_write_bytecode = True
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('home.html') 
+  return render_template('home.html', listings = storedListings.listings) 
 
 @app.route('/listing')
 def listing():
@@ -29,6 +30,7 @@ def create_listing():
     image_filename = image_file.filename
     image_path =os.path.join('static/img', image_filename)
     image_file.save(image_path)
+    storedListings.listings.append({"beds":beds,"baths":baths,"sqft":sqft,"location":location,"description":description,"rent":rent,"imageName":image_filename})
     return render_template('listing.html', beds=beds, baths=baths, sqft=sqft, location=location, rent=rent, description=description, filename=image_filename)
 
 
