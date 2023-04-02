9d6f44a28f2cd36be40341d8b1133b3631278d41
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-04-01 23:49:20 -0500

    added login functionality

---
+++ data/users.py
@@ -0,0 +1,4 @@
+
+users = {
+    'test@gmail.com':['sha256$8jvR49qcskOgV945$325c7aaa72bdbe0d83a700028b54d4e5e7ab229a1e4f48f677e921727f056bb3','test']
+}
--- routes.py
+++ routes.py
@@ -1,10 +1,18 @@
 import sys 
 import os
-from data import storedListings
+from data import storedListings, users
+from secret import keys
 sys.dont_write_bytecode = True
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
 
 app = Flask(__name__)
+app.secret_key= keys.flaskey
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
 
 @app.route('/')
 def home():
@@ -34,6 +42,91 @@ def create_listing():
     return render_template('listing.html', beds=beds, baths=baths, sqft=sqft, location=location, rent=rent, description=description, filename=image_filename)
 
 
+@app.route('/account')
+@login_required
+def account():
+   return render_template('account.html')
+
+#login implementation
+class User(UserMixin):
+    name: str
+    passwordHash: str
+    def __init__(self, email, password, name):
+        self.id = email
+        self.name = name
+        self.passwordHash = generate_password_hash(password,method='sha256')
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+
+
+@login_manager.user_loader
+def load_user(email):
+    if(email in users.users):
+       return User(email, users.users[email][0], users.users[email][1])
+    return None
+
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    print(email,name,password)
+
+    # if this returns a user, then the email already exists in database
+    if email in users.users:
+      print(email,name)
+      flash('Email address already exists')
+      return render_template('signup.html')
+
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    users.users[email] = [generate_password_hash(password, method='sha256'),name]
+
+    # add the new user to the database TO DO
+
+    return render_template('login.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    # check if user actually exists
+    if email not in users.users:
+      print(email)
+      flash('Email address not found ')
+      return render_template("login.html")
+    
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not check_password_hash(users.users[email][0], password):
+        flash('Please check your login details and try again.')
+        return render_template('login.html')  # if user doesn't exist or password is wrong, reload the page
+
+    # # if the above check passes, then we know the user has the right credentials
+    user = User(email, users.users[email][0], users.users[email][1])
+    login_user(user, remember=remember)
+    return redirect(url_for('home'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
