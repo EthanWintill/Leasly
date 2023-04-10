8167b0fe1d470dfa16638c2c541ac08e494f63d3
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-04-09 19:29:28 -0500

    getting dis guy ready for da deployment

--- frontend/src/Components/ApartmentView.js
+++ frontend/src/Components/ApartmentView.js
@@ -9,7 +9,7 @@ import './ApartmentView.css'
 const ApartmentView = () => {
     const { id } = useParams();
     const [data, setdata] = useState({
-        listings: [{}]
+        listings: []
     });
     /*needed functions
         - api call to flask backend, gets apartment based on a search for id, returns this in json format, then renders page with apartments info such as listings and ammenities 
@@ -23,8 +23,6 @@ const ApartmentView = () => {
     useEffect(() => {
         fetch("/api/sublets?apartment=" + id).then((res) =>
             res.json().then((data) => {
-                // Setting a data from api
-                // only one listing rn, feel free to add some
                 setdata({
                     listings: data
                 });
@@ -40,57 +38,54 @@ const ApartmentView = () => {
             */}
             <Navbar></Navbar>
             <div className="aptmntViewContainer">
-                        
-                        <div className="apartmentInfoContainer">
-                            
-                                <img className="apartmentImage" src={testur}></img>
-                            
-                            <div className="apartmentInfo">
-                                <p className="apartmentName"> {id}</p>
-                                <p> rating</p>
-                                <p> 4.5/5</p>
-                                <p> apartment link</p>
-                                <a> a link to the apartment website </a>
-                                <p> (512) - 512-5123</p>
-                                <p> amenities</p>
-                                <div className="amenitiesContainer">
-                                    {/* 9 divs acting as cards, one for each amenitie, arranged in a flex container*/}
-                                    <div>
-                                        <span class="material-symbols-outlined"> pets </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> spa </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> sports_gymnastics </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> water_ec </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> directions_bus </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> self_improvement </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> local_laundry_service </span>
-                                    </div>
-                                    <div>
-                                        <span class="material-symbols-outlined"> bed </span>
-                                    </div>
-                                </div>
+                <div className="apartmentInfoContainer">
+                    <img className="apartmentImage" src={testur}></img>
+                    <div className="apartmentInfo">
+                        <p className="apartmentName"> {id}</p>
+                        <p> rating</p>
+                        <p> 4.5/5</p>
+                        <p> apartment link</p>
+                        <a> a link to the apartment website </a>
+                        <p> (512) - 512-5123</p>
+                        <p> amenities</p>
+                        <div className="amenitiesContainer">
+                            {/* 9 divs acting as cards, one for each amenitie, arranged in a flex container*/}
+                            <div>
+                                <span class="material-symbols-outlined"> pets </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> spa </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> sports_gymnastics </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> water_ec </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> directions_bus </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> self_improvement </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> local_laundry_service </span>
+                            </div>
+                            <div>
+                                <span class="material-symbols-outlined"> bed </span>
                             </div>
                         </div>
-                        <div className="subleaseViewContainer">
-                            <AptmntSubleaseBoard identifier={data.listings}/>
-                        </div>
-                        
-                        <div className="reviewViewContainer">
-                            <AptmntReviews identifier={id}/>
-                        </div>
-                        
                     </div>
+                </div>
+                <div className="subleaseViewContainer">
+                    <AptmntSubleaseBoard listings={data.listings} />
+                </div>
+
+                <div className="reviewViewContainer">
+                    <AptmntReviews identifier={id} />
+                </div>
+
+            </div>
         </div>
     )
 
--- frontend/src/Components/AptmntSubleaseBoard.js
+++ frontend/src/Components/AptmntSubleaseBoard.js
@@ -1,36 +1,35 @@
 import './AptmntSubleaseBoard.css'
 
 
-const AptmntSubleaseBoard = ({listings}) =>{
-    
-    return(
-        
+const AptmntSubleaseBoard = ({ listings }) => {
+    console.log(listings)
+    return (
+
         <div className="bountyContainer">
             <p className="bountyTitle">Subleases Available</p>
-                <div className="bountyView">
-                {/* uncomment the following comments when backend is online, will instantly populate with correct data for each listing
-                {listings.map((sublet) =>*/}
-                    <div className="bountyCards">
-                        <img className="bountyImage"></img>
-                        <div className="bountyGeneral">
-                            <p> Floor Plan </p>
-                            <p> Price</p>
-                            <p> Duration</p>
-                        </div>
-                        <div className="bountyDescription">
-                            {/*<p> {sublet.description}</p>*/}
-                            <p> Lorem ipsum dolor sit amet, consectetur 
-                                adipiscing elit. Nunc maximus, nulla ut commodo sagittis, 
-                                sapien dui mattis dui, non pulvinar lorem felis nec erat
-                                r sit amet, consectetur 
-                                adipiscing elit. Nunc maximus, nulla ut commodo sagittis, 
-                                sapien dui mattis dui, non pulvinar lorem felis nec erat</p>
-                            <button> Message </button>
-                        </div>
-                        
+            <div className="bountyView">
+                {listings.map((sublet) =>
+                <div className="bountyCards">
+                    <img className="bountyImage"></img>
+                    <div className="bountyGeneral">
+                        <p> Floor Plan: {sublet.bed} bed, {sublet.bath} bath</p>
+                        <p> Price: ${sublet.rent}/month</p>
+                        <p> Duration:  </p>
                     </div>
-                {/*)}*/}
+                    <div className="bountyDescription">
+                        <p> {sublet.description}</p>
+                        <p> Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit. Nunc maximus, nulla ut commodo sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat
+                            r sit amet, consectetur
+                            adipiscing elit. Nunc maximus, nulla ut commodo sagittis,
+                            sapien dui mattis dui, non pulvinar lorem felis nec erat</p>
+                        <button> Message </button>
+                    </div>
+
                 </div>
+                )}
+            </div>
         </div>
     )
 }
