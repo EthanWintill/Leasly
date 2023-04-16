c955e7242de467fe951b4e7e2365b87d6c363b05
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-04-16 01:35:26 -0500

    added support for reviews!

--- frontend/app/pages/account/ProfilePage.js
+++ frontend/app/pages/account/ProfilePage.js
@@ -4,20 +4,30 @@ import {auth} from '../../util/FirebaseFuncs';
 export default function ProfilePage() {
   const [data, setdata] = useState({
     listings: [],
+    reviews: [],
   });
 
   // Using useEffect for single rendering
   useEffect(() => {
-    // Using fetch to fetch the api from
-    // flask server it will be redirected to proxy
+    // Using fetch to fetch the listings API
     fetch('https://leaslybackend.herokuapp.com/api/sublets?user=' + auth.currentUser.displayName).then((res) =>
       res.json().then((sublets) => {
-        // Setting a data from api
-        // only one listing rn, feel free to add some
-        setdata({
+        // Setting data for listings
+        setdata((prevData) => ({
+          ...prevData,
           listings: sublets,
-        });
-        console.log(data);
+        }));
+      }),
+    );
+
+    // Using fetch to fetch the reviews API
+    fetch('https://leaslybackend.herokuapp.com/api/review?name=' + auth.currentUser.displayName).then((res) =>
+      res.json().then((reviews) => {
+        // Setting data for reviews
+        setdata((prevData) => ({
+          ...prevData,
+          reviews: reviews,
+        }));
       }),
     );
     // eslint-disable-next-line
@@ -29,15 +39,25 @@ export default function ProfilePage() {
       <h1 style={{color: 'black'}}>Your listings</h1>
       <div className="listings">
         <ul>
-
           {data.listings.map((sublet) =>
             <div key={sublet.rent}>
               <li>Listing description: {sublet.description}</li>
             </div>,
           )}
-
         </ul>
+      </div>
 
+      <h1 style={{color: 'black'}}>Your reviews</h1>
+      <div className="reviews">
+        <ul>
+          {data.reviews.map((review) =>
+            <div key={review.id}>
+              <li>Apartment name: {review.apartment_name}</li>
+              <li>Rating: {review.rating}</li>
+              <li>Comment: {review.comment}</li>
+            </div>,
+          )}
+        </ul>
       </div>
     </div>
   );
